fa12e0e8e460b241aed5f6f1f6c01b7b22e8f516
jnjagod <josh.jagoda@gmail.com>
2019-05-21 15:55:26 -0600

    app-4 done

---
+++ app-4/src/components/login.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+
+export default class Login extends Component {
+    constructor(){
+        super()
+
+        this.state = {
+            user: '',
+            pass: ''
+        }
+    }
+
+    userUpdate = name => {
+        this.setState({user: name})
+    }
+
+    passUpdate = pass => {
+        this.setState({pass: pass})
+    }
+
+    handleAlert = () => {
+        alert(`Username: ${this.state.user} Password: ${this.state.pass}`)
+    }
+
+    render () {
+        return (
+            <div>
+                <input onChange={e => this.userUpdate(e.target.value)} type="text"></input>
+                <input onChange={e => this.passUpdate(e.target.value)} type="text"></input>
+                <button onClick={this.handleAlert}>login</button>
+            </div>
+        )
+    }
+}
