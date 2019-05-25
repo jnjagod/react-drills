41c39fa08872ac22e778301fc27c4f2d35f72227
jnjagod <josh.jagoda@gmail.com>
2019-05-24 21:27:02 -0600

    #7

---
+++ app-7/src/components/NewTask.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class NewTask extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            input: ''
+        }
+    }
+
+    handleInput = value => this.setState({input: value})
+
+    handleAdd = () => {
+        this.props.add(this.state.input)
+        this.setState({input: ''})
+    }
+
+    render() {
+        return (
+            <div>
+                <input value={this.state.input} onChange={e => this.handleInput(e.target.value)}></input>
+                <button onClick={this.handleAdd}>Add</button>
+            </div>
+        )
+    }
+
+}
